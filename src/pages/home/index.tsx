a9ce6326d056d3d0719eb9198dfaaf566fed5f5a
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-02-22 08:22:25 +0200

    handle structure

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import FirstComponent from '@/components/templates/home/firstComponent'
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+        <FirstComponent/>
+    </div>
+  )
+}
+
+export default index
