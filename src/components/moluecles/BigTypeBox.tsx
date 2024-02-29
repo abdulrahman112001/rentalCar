ada8988d4cea866feaa4cd75678a0f9b9e39edcd
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-02-29 22:55:34 +0200

    updates

--- src/components/moluecles/BigTypeBox.tsx
+++ src/components/moluecles/BigTypeBox.tsx
@@ -1,24 +1,34 @@
-import Image from 'next/image'
-import React from 'react'
-import { TypeBox } from '.'
-import { Electric, HATCH, JEEP, Luxary, Sedan, Sports,  VAN } from '../atom/assets'
+import Image from "next/image";
+import React from "react";
+import { TypeBox } from ".";
+import {
+  Electric,
+  HATCH,
+  JEEP,
+  Luxary,
+  Sedan,
+  Sports,
+  VAN,
+} from "../atom/assets";
 
 export default function BigTypeBox() {
   return (
     <div className="big-box flex flex-wrap justify-between items-center gap-y-10 my-14">
-                    <div className="type-box w-full sm:w-[47%]  lg:w-[22%] relative flex justify-center items-center">
-                        {/* <Image src={Suv} alt="" className="min-w-[200px] min-h-[90px]" /> */}
-                        <span className=" font-medium text-xl leading-6 absolute bottom-[15%] left-1/2 -translate-x-1/2 text-white">
-                            SUV
-                        </span>
-                    </div>
-                    <TypeBox image={Sports} type="Sports" />
-                    <TypeBox image={Luxary} type="Luxary" />
-                    <TypeBox image={Electric} type="Electric" />
-                    <TypeBox image={VAN} type="VAN" />
-                    <TypeBox image={JEEP} type="JEEP" />
-                    <TypeBox image={Sedan} type="Sedan" />
-                    <TypeBox image={HATCH} type="HATCH Back" />
-                </div>
-  )
+      <div className="type-box w-full sm:w-[47%]  lg:w-[22%] relative flex justify-center items-center">
+        {/* <Image src={Suv} alt="" className="min-w-[200px] min-h-[90px]" /> */}
+        <span className=" font-medium text-xl leading-6 absolute bottom-[15%] left-1/2 -translate-x-1/2 text-white">
+          SUV
+        </span>
+      </div>
+      <TypeBox image={HATCH} type="HATCH Back" />
+
+      <TypeBox image={Sports} type="Sports" />
+      <TypeBox image={Luxary} type="Luxary" />
+      <TypeBox image={Electric} type="Electric" />
+      <TypeBox image={VAN} type="VAN" />
+      <TypeBox image={JEEP} type="JEEP" />
+      <TypeBox image={Sedan} type="Sedan" />
+      <TypeBox image={HATCH} type="HATCH Back" />
+    </div>
+  );
 }
